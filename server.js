86795e0db81d6e359eb5029645f2e28e52798a4f
Kenny Wai <97656734+KennyWCoder@users.noreply.github.com>
2022-11-20 15:52:06 -0800

    update demo video

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require("console.table");
+require("console.table");
 
 const db = mysql.createConnection(
     {
